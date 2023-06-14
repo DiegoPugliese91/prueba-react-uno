5aeac7e9b05295e1774dab4c2c6a12964651439e
Diego Pugliese <diego_pugliese91@hotmail.com>
2023-06-14 15:01:13 -0300

    modificacion de archivos

--- src/App.js
+++ src/App.js
@@ -5,12 +5,8 @@ import PrimerComponente from './components/PrimerComponente';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <PrimerComponente />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <header className="App-header">        
+        <PrimerComponente />        
       </header>
     </div>
   );
--- src/components/PrimerComponente.js
+++ src/components/PrimerComponente.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const PrimerComponente = () => {
+
+  //let nombre = "Diego";
+  let apellido = "Pugli";
+  
+  const [nombre,setNombre] = useState("Diegol");
+
+  let cursos= 
+  [
+    "Javascript",
+    ".Net",
+    "sql"
+  ]   
+
+  function cambiarNombre(nuevoNombre)
+  {
+    setNombre(nuevoNombre);
+  }
+
+  function handleChange(e) {
+    cambiarNombre(e.target.value);
+  }
+
   return (
     <div>
         <h1>Hola Comp1</h1>
+        <p>Mi nombre es: {nombre}</p>
+        <p>Mi apellido es: {apellido}</p>
+        
+        <input type="text" onChange={handleChange} />
+
+        <button onClick={() => cambiarNombre("pepe")}>Cambiar nombre</button>
+
+        <div>
+         <h2>Lista de Cursos</h2>
+         <ul>
+           {cursos.map((curso, index) => (
+           <li key={index}>{curso}</li>
+           ))}
+           </ul>
+    </div>
+
     </div>
   )
 }
